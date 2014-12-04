3a49b12c95244b58e2e73924b6872d77a49b3f48
Erik <erik.svedang@gmail.com>
2014-12-04 10:54:37 +0100

    first

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/fears-standalone.jar clojure.main -m fears.web
---
+++ test/fears/web_test.clj
@@ -0,0 +1,6 @@
+(ns fears.web-test
+  (:require [clojure.test :refer :all]
+            [fears.web :refer :all]))
+
+(deftest first-test
+  (is false "Tests should be written"))
